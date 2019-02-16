b497ff60fc29b78fc629c10063b77500a43707c7
Ray Lomeli <raylom96@gmail.com>
2019-02-16 16:29:01 -0600

    changed authentication reducer path in root reducer to fix compike error

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 ReactDOM.render(
   <Provider store={configureStore()}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
-serviceWorker.unregister();
+// serviceWorker.unregister();
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import authenticationReducer from "./authenticationReducer";
+import authenticationReducer from "../src/components/authentication/authenticationReducer";
 export default combineReducers({
   authenticationReducer
 });
