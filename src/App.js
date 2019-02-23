6f9e251bcbe123e9f52985a42437f79e5fc9282d
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-23 12:41:45 -0600

    Redux form connected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import AuthForm from './components/authentication/AuthForm';
 
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">
-        Hello Sky Lambda
-      </Button>
+      <>
+        <AuthForm />
+        {/* <Button variant='contained' color='primary'>
+          Hello Sky Lambda
+        </Button> */}
+      </>
     );
   }
 }
--- src/components/authentication/AuthForm.js
+++ src/components/authentication/AuthForm.js
@@ -1,1 +1,69 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Field, reduxForm } from 'redux-form';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const ButtonWrapper = styled.div`
+  margin-top: '30px';
+`;
+
+class AuthForm extends React.Component {
+  renderInput = ({ input, label, meta: { error, touched } }) => {
+    return (
+      <div className="container">
+        <p>{label}</p>
+        <TextField
+          error={error && touched ? true : false}
+          id={`outlined-${input.name}-input`}
+          type={`${input.name}`}
+          variant="outlined"
+          name={`${input.name}`}
+          {...input}
+          autoComplete="off"
+          label={label}
+          margin="normal"
+        />
+        {error && touched ? <p>{error}</p> : null}
+      </div>
+    );
+  };
+
+  onFormSubmit = formValues => console.log(formValues);
+
+  render() {
+    return ReactDOM.createPortal(
+      <form onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
+        <Field
+          name="email"
+          component={this.renderInput}
+          label="Email Address"
+        />
+        <Field name="password" component={this.renderInput} label="Password" />
+        <ButtonWrapper>
+          <Button type="submit" variant="contained" color="primary">
+            Register
+          </Button>
+        </ButtonWrapper>
+      </form>,
+      document.querySelector('#auth-modal')
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.email) {
+    errors.email = 'You must enter a name, this field is required!';
+  }
+  if (!formValues.password) {
+    errors.password = 'You must enter an email, this field is required!';
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: 'SignIn',
+  validate
+})(AuthForm);
